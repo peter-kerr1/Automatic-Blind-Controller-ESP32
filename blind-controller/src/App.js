a8e0c70c788952425111a4f873df3c96d13b596d
Peter Kerr <peter.kerr@outlook.com>
2021-07-28 11:42:30 +1000

    Frontend menu created

--- blind-controller/src/App.js
+++ blind-controller/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import StopIcon from '@material-ui/icons/Stop';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>ESP32 Blind Controller</h2>
+        <Buttons />
       </header>
     </div>
   );
 }
 
-export default App;
+function Buttons() {
+  const [state, setState] = React.useState('stop');
+
+  const handleChange = (event, newState) => {
+    if (newState !== null) {
+      setState(newState);
+    }
+  };
+
+  return (
+    <ToggleButtonGroup orientation="vertical" size="large" value={state} onChange={handleChange} exclusive>
+      <ToggleButton value="up">
+        <KeyboardArrowUpIcon/>
+      </ToggleButton>
+      <ToggleButton value="stop">
+        <StopIcon/>
+      </ToggleButton>
+      <ToggleButton value="down">
+        <KeyboardArrowDownIcon/>
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
