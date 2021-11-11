0317d00be5a19949fba36986c0c3d1494f07359e
Peter Kerr <peter.kerr@outlook.com>
2021-11-11 17:33:25 +1100

    Add code to update Firebase with current lux value

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -15,6 +15,9 @@ FirebaseData firebaseIO;
 #define SENS2 27
 Motor motor(IN1, IN2, SENS1, SENS2);
 
+#include "Adafruit_VEML7700.h"
+Adafruit_VEML7700 lightSensor = Adafruit_VEML7700();
+
 void setup() {
   Serial.begin(115200);
   initWifi(WIFI_SSID, WIFI_PASSWORD);
@@ -22,25 +25,37 @@ void setup() {
   initFirebase(DATABASE_URL, DATABASE_SECRET);
   addListener(commandStream, BLIND_NAME"/command", commandListener);
   initMotorEncoder();
+  initLightSesnor();
 }
 
 
 unsigned long timeElapsed = 0;
 int prevEncoderVal = 0;
+int prevLuxVal = 0;
 void loop() {
   if (motor.halt) {
     // Can't afford for this set call to fail, keep attempting until success
     while (!Firebase.setStringAsync(firebaseIO, BLIND_NAME"/command", "stop"));
     motor.halt = false;
   }
-  // Update encoder value on Firebase every 200ms if it has changed, so that in the event of
-  // a power failure the motor position can be restored when the ESP32 is powered back on.
+  
+  // Every 200ms:
+  // - Update encoder value on Firebase if it has changed, so that in the event of
+  //   a power failure the motor position can be restored when the ESP32 is powered back on.
+  // - Update lux value on Firebase if it has changed.
+  // NOTE:
   // millis() overflows back to zero after approximately 50 days of continuous execution,
   // hence the last condition.
-  if (motor.encoderVal != prevEncoderVal && (millis() - timeElapsed > 200 || millis() - timeElapsed < 0)) {
-    Firebase.setIntAsync(firebaseIO, BLIND_NAME"/encoderVal", motor.encoderVal);
+  if (millis() - timeElapsed > 200 || millis() - timeElapsed < 0) {
+    if (motor.encoderVal != prevEncoderVal) {
+      Firebase.setIntAsync(firebaseIO, BLIND_NAME"/encoderVal", motor.encoderVal);
+      prevEncoderVal = motor.encoderVal;
+    }
+    if (lightSensor.readLux() != prevLuxVal) {
+      Firebase.setIntAsync(firebaseIO, BLIND_NAME"/currentLux", lightSensor.readLux());
+      prevLuxVal = lightSensor.readLux();
+    }
     timeElapsed = millis();
-    prevEncoderVal = motor.encoderVal;
   }
 }
 
@@ -69,4 +84,14 @@ void initMotorEncoder() {
   Firebase.getInt(firebaseIO, BLIND_NAME"/encoderMax", motor.encoderMax);
   Firebase.getInt(firebaseIO, BLIND_NAME"/encoderVal", motor.encoderVal);
   attachInterrupt(digitalPinToInterrupt(SENS1), [](){ motor.encoderCallback(); }, RISING);
+}
+
+
+// Set up light sensor with gain and IT such that it can read lux values between 0-30,000lx
+void initLightSesnor() {
+  Serial.print("Initialising light sensor... ");
+  while (!lightSensor.begin());
+  lightSensor.setGain(VEML7700_GAIN_1_4);
+  lightSensor.setIntegrationTime(VEML7700_IT_50MS);
+  Serial.println("done");
 }
