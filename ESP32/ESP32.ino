cb552efd7128d6696ea5236d6b6e3249f9a876c5
Peter Kerr <peter.kerr@outlook.com>
2022-01-18 16:53:20 +1100

    Add automatic raise and lower functionality

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -6,8 +6,9 @@
 
 // Uses v3.10.5 of Firebase ESP32 Client by Mobizt
 #include <FirebaseESP32.h>
-FirebaseData commandStream;
 FirebaseData firebaseIO;
+FirebaseData settingStream;
+String settings[3] = {"/command", "/lowerTime", "/raiseTime"};
 
 #include "motor.h"
 #define IN1 33
@@ -20,22 +21,28 @@ Motor motor(IN1, IN2, SENS1, SENS2);
 #include "Adafruit_VEML7700.h"
 Adafruit_VEML7700 lightSensor = Adafruit_VEML7700();
 
+#include "time.h"
+#define HOUR_IN_SEC 3600
+
 void setup() {
   Serial.begin(115200);
   initWifi(WIFI_SSID, WIFI_PASSWORD);
+  configTime(11 * HOUR_IN_SEC, HOUR_IN_SEC, "pool.ntp.org");
   initLightSensor();
-  Serial.printf("\nFirebase Client v%s\n", FIREBASE_CLIENT_VERSION);
   initFirebase(DATABASE_URL, DATABASE_SECRET);
   initMotorEncoder();
-  addListener(commandStream, BLIND_NAME"/settings/command", commandListener);
+  addMultiPathListener(settingStream, BLIND_NAME"/settings", settings, settingListener);
 }
 
 
 
-unsigned long timeElapsed = 0;
-int prevEncoderVal = 0;
+unsigned long sensorTimer = 0;
+unsigned long blindTimer = 0;
+int prevEncoderVal = -1;
 int prevLux = 0;
 int currentLux = 0;
+int raiseTime = -1;
+int lowerTime = -1;
 
 void loop() {
   if (motor.halt) {
@@ -43,6 +50,7 @@ void loop() {
     while (!Firebase.setStringAsync(firebaseIO, BLIND_NAME"/settings/command", "stop"));
     motor.halt = false;
   }
+  
   // Every 200ms:
   // - Update encoder value on Firebase if it has changed, so that in the event of
   //   a power failure the motor position can be restored when the ESP32 is powered back on.
@@ -50,39 +58,71 @@ void loop() {
   // NOTE:
   // millis() overflows back to zero after approximately 50 days of continuous execution,
   // hence the last condition.
-  if (millis() - timeElapsed > 200 || millis() - timeElapsed < 0) {
+  if (millis() - sensorTimer >= 200 || millis() - sensorTimer < 0) {
     if (motor.encoderVal != prevEncoderVal) {
       Firebase.setIntAsync(firebaseIO, BLIND_NAME"/encoderVal", motor.encoderVal);
       prevEncoderVal = motor.encoderVal;
     }
-    
     currentLux = lightSensor.readLux();
     if (currentLux != prevLux) {
       Firebase.setIntAsync(firebaseIO, BLIND_NAME"/currentLux", currentLux);
       prevLux = currentLux;
     }
-    
-    timeElapsed = millis();
+    sensorTimer = millis();
+  }
+  
+  // Every 15 seconds, check to see if it's time to raise or lower the blinds.
+  // If it is, we update the command on Firebase so that the GUI syncs up with what the blind is doing.
+  if (millis() - blindTimer >= 15000 || millis() - blindTimer < 0) {
+    int currentTime = getTime();
+    if (currentTime == raiseTime) {
+      while (!Firebase.setStringAsync(firebaseIO, BLIND_NAME"/settings/command", "up"));
+    } else if (currentTime == lowerTime) {
+      while (!Firebase.setStringAsync(firebaseIO, BLIND_NAME"/settings/command", "down"));
+    }
+    blindTimer = millis();
   }
 }
 
 
 
+// Returns the current time as an integer in 24-hour format (eg: 6:31pm -> 1831)
+int getTime() {
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("[ERROR] Failed to obtain current time");
+    return 0;
+  } else {
+    return timeinfo.tm_hour * 100 + timeinfo.tm_min;
+  }
+}
+
 // Controls the motor based on new command received from Firebase
-void commandListener(StreamData data) {
-  if (data.dataType() == "string") {
-    String command = data.stringData();
-    Serial.printf("Command read: '%s'\n", command);
-    if (command == "up") {
-      motor.clockwise();
-    } else if (command == "down") {
-      motor.antiClockwise();
-    } else {
-      motor.stop();  // Stop on unexpected input as well as 'stop' command
+void executeCommand(String command) {
+  if (command == "up") {
+    motor.clockwise();
+  } else if (command == "down") {
+    motor.antiClockwise();
+  } else {
+    motor.stop();  // Stop on unexpected input as well as 'stop' command
+  }
+}
+
+// 
+void settingListener(MultiPathStreamData node) {
+  for (String setting : settings) {
+    if (node.get(setting)) {
+      Serial.printf("New %s received: '%s'\n", &setting[1], node.value);
+      if (setting == "/command") {
+        executeCommand(node.value);
+      } else if (setting == "/lowerTime") {
+        lowerTime = node.value.toInt();
+      } else if (setting == "/raiseTime") {
+        raiseTime = node.value.toInt();
+      }
     }
   }
 }
-
 
 // Sync encoder values with those stored on Firebase
 // Register an interrupt to count encoder ticks
--- ESP32/Firebase.ino
+++ ESP32/Firebase.ino
@@ -1,6 +1,7 @@
 #include <FirebaseESP32.h>
 
 void initFirebase(char* url, char* secret) {
+  Serial.printf("\nFirebase Client v%s\n", FIREBASE_CLIENT_VERSION);
   Serial.print("Connecting to Firebase... ");
   Firebase.reconnectWiFi(true);
   Firebase.begin(url, secret); //Legacy authentication method because I'm lazy
@@ -11,12 +12,12 @@ void genericTimeoutCallback(bool timeout) {
   if (timeout) Serial.println("Stream timeout occurred, resuming...\n");
 }
 
-void addListener(FirebaseData &stream, char* path, FirebaseData::StreamEventCallback callbackFn) {
-  Serial.printf("Attaching listener to '%s'... ", path);
-  if (!Firebase.beginStream(stream, path)) {
+void addMultiPathListener(FirebaseData &stream, String parentPath, String* childPaths, FirebaseData::MultiPathStreamEventCallback callbackFn) {
+  Serial.printf("Attaching listener to '%s'... ", parentPath.c_str());
+  if (!Firebase.beginMultiPathStream(stream, parentPath, childPaths, sizeof(childPaths)/sizeof(childPaths[0]))) {
     Serial.printf("\nFailed to begin stream: %s\n\n", stream.errorReason());
   } else {
-    Firebase.setStreamCallback(stream, callbackFn, genericTimeoutCallback);
+    Firebase.setMultiPathStreamCallback(stream, callbackFn, genericTimeoutCallback);
     Serial.println("done");
   }
 }
